dce62b84b6a61feda8fd626c259d90d4311c4324
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2014-11-10 12:44:06 +0000

    [#3] Baseline Logging

---
+++ branches/dev+IB0003-BaselineLogging/src/lib/core/log/ForkOutputBehavior.cpp
@@ -0,0 +1,9 @@
+#include "ForkOutputBehavior.h"
+
+#include <type/CharCode.h>
+
+ForkOutputBehavior::ForkOutputBehavior(const BasicNameList & schemeName,
+                                       const BasicName metaName)
+    : mWritable(false)
+{
+}
---
+++ branches/dev+IB0003-BaselineLogging/src/lib/support/var/Version.h
@@ -0,0 +1,12 @@
+/*! @file   var/Version.h   Version info for eirVar library
+*
+*/
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_FILETYPE    VFT_DLL
+#define VER_APPNAME     "eirVar"
+#define VER_FILEDESC    "EclipseIR Variable Container Library"
+
+#endif // VERSION_H
