b4559feab3cf96d2c0bc8f645c48ccd98ba6ee7d
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2014-11-20 05:56:51 +0000

    Start major re-factor of LogFork, ForkOutputBehavior, and BuiltinOutputBehavior classes. [#9]

--- src/lib/core/log/LogFork.cpp
+++ src/lib/core/log/LogFork.cpp
@@ -26,3 +26,22 @@ void LogFork::close(void)
 {
     mpOutput->close();
 }
+
+bool LogFork::parseUrl(const QUrl & url)
+{
+    if ( ! url.isValid()) return false;
+    mUrl = url;
+    mSchemeEcc = EightCC(url.scheme());
+    //ASSERT(mRegisteredEccs.contains(mSchemeEcc));
+    const QUrlQuery query(url);
+    typedef QPair<QString, QString> QStringPair;
+    const QList<QStringPair> items(query.queryItems());
+    foreach (QStringPair item, items)
+    {
+        BasicName name(item.first);
+        QString value(item.second);
+        mUrlOptionMap.insert(name, value);
+    }
+
+    return true;
+}
--- src/lib/core/log/LogFork.h
+++ src/lib/core/log/LogFork.h
@@ -6,6 +6,7 @@
 
 #include <base/BasicName.h>
 #include <base/BasicNameHash.h>
+#include <type/CharCode.h>
 
 class ForkOutputBehavior;
 class ItemFormatBehavior;
@@ -16,6 +17,7 @@ class LogFork
 public:
     LogFork(const BasicName & name,
             const QUrl & url);
+    bool isWritable(void) const;
     bool start(void);
     bool isStarted(void) const;
     bool isError(void) const;
@@ -24,19 +26,27 @@ public:
     void close(void);
 
 private:
+    bool parseUrl(const QUrl & url);
+
+private:
+    bool mIsStarted = false;
+    QString mErrorString;
+    bool mWritable = false;
+
     BasicName mName;
+    EightCC mSchemeEcc;
     QUrl mUrl;
     QUrlQuery mQuery;
-    QString mErrorString;
+    BasicNameHash<QString> mUrlOptionMap;
+
     BasicName mFormatName;
-    bool mIsStarted = false;
-    QString mLineEndString = (QChar(QChar::CarriageReturn)
-                              + QChar(QChar::LineFeed));
     bool mAutoFlush = false;
     bool mReportPass = false;
     ForkOutputBehavior * mpOutput = 0;
     ItemFormatBehavior * mpFormat = 0;
     //LogSeverityFilter mSeverityFilter;
+
+
 };
 
 #endif // LOGFORK_H
