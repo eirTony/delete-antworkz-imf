1782e9ef924d16684da4863deaa05d2dc2f8dd76
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2014-11-28 02:25:05 +0000

    Start to flesh out LogItem functions. [#3]

--- src/lib/core/log/Severity.h
+++ src/lib/core/log/Severity.h
@@ -10,15 +10,18 @@ public:
 
 public:
     Severity(const int iLevel=0);
-    Severity(const QString & name);
+    Severity(const BasicName & name);
     BasicName name(void) const;
 //    operator bool (void) const;
     operator int  (void) const;
+    Severity pass(void) const;
+    bool is(const BasicName & name) const;
     bool operator == (const Severity other) const;
     bool operator >  (const Severity other) const;
 
 public: // static functions
     static void staticCtor(void);
+    static Severity pass(const Severity fail);
 
 private:
     int mLevelInt;
