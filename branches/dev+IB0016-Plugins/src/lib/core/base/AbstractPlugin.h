1bdfd3bb6c7743d7e80ba0ef31bbe7ceb4d782a5
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2014-12-02 05:21:10 +0000

    Finish ctor train. [#16]

--- branches/dev+IB0016-Plugins/src/lib/core/base/AbstractPlugin.h
+++ branches/dev+IB0016-Plugins/src/lib/core/base/AbstractPlugin.h
@@ -1,12 +1,21 @@
 #ifndef ABSTRACTPLUGIN_H
 #define ABSTRACTPLUGIN_H
+#include "BaseLib.h"
 
 #include "ModuleInfo.h"
 
-class AbstractPlugin : public ModuleInfo
+#include "BasicName.h"
+
+class BasicId;
+
+class BASESHARED_EXPORT AbstractPlugin : public ModuleInfo
 {
 public:
-    AbstractPlugin();
+
+protected:
+    AbstractPlugin(const BasicId & prefix,
+                   const BasicName & name,
+                   const BasicNameList & aliases);
 };
 
 #endif // ABSTRACTPLUGIN_H
