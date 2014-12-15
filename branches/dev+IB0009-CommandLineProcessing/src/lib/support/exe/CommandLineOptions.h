092543dabe140793afb3d5e361cef168658f28a3
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2014-12-15 07:28:39 +0000

    Fleshing out CommandLineOptions. [#9]

--- branches/dev+IB0009-CommandLineProcessing/src/lib/support/exe/CommandLineOptions.h
+++ branches/dev+IB0009-CommandLineProcessing/src/lib/support/exe/CommandLineOptions.h
@@ -1,25 +1,23 @@
 #ifndef COMMANDLINEOPTIONS_H
 #define COMMANDLINEOPTIONS_H
+#include "ExeLib.h"
 
 #include <base/BasicNameMap.h>
 #include "CommandLineOption.h"
 
-class CommandLineOptions : private BasicNameMap<CommandLineOption>
+class EXESHARED_EXPORT CommandLineOptions
 {
 public:
     CommandLineOptions(void);
     bool isEmpty(void) const;
     bool isValid(void) const;
     void clear(void);
-    void addPositionalArgument(const BasicName & argName,
-                               const QQString description,
-                               const QQString syntax=QQString());
-    bool add(const BasicName & optName,
-             const CommandLineOption option);
-    bool remove(const BasicName & key);
+    bool add(const CommandLineOption option);
+    bool remove(const BasicName & name);
 
 private:
     BasicName::List mPositionalNameList;
+    BasicNameMap<CommandLineOption> mOptionMap;
 };
 
 #endif // COMMANDLINEOPTIONS_H
