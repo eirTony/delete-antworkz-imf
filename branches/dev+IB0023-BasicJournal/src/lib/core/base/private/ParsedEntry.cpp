10fabc03af28c12fec509ad9eee6320b80b818ee
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2015-02-26 22:29:45 +0000

    Finish ParsedEntry; more meat for Basic Journal. [#23]

--- branches/dev+IB0023-BasicJournal/src/lib/core/base/private/ParsedEntry.cpp
+++ branches/dev+IB0023-BasicJournal/src/lib/core/base/private/ParsedEntry.cpp
@@ -1,21 +1,27 @@
 #include "ParsedEntry.h"
 
 ParsedEntry::ParsedEntry(const BasicJournalEntry & entry)
+    : timeStamp(entry.value("TimeStamp").toDateTime())
+    , macroName(BasicJournalQueue::parseMacroName
+                        (entry.value("MacroName").toString()))
+    , category(BasicJournalQueue::parseCategory
+                        (entry.value("Category").toString()))
+    , message(entry.value("Message").toString())
+    , expression(entry.value("Expression").toString())
+    , objectName(entry.value("ObjectName").toString())
+    , var1(entry.value("Var1"))
+    , var2(entry.value("Var2"))
+    , var3(entry.value("Var3"))
+    , var4(entry.value("Var4"))
 {
-    set(entry);
 }
 
-void ParsedEntry::set(const BasicJournalEntry & entry)
+QString ParsedEntry::formatted(void) const
 {
-    timeStamp = entry.value("TimeStamp").toDateTime();
-}
-
-int ParsedEntry::variantSize(void) const
-{
-
-}
-
-QVariant ParsedEntry::variant(const int x) const
-{
-
+    QString rtn(message);
+    rtn.replace("%1", var1.toString());
+    rtn.replace("%2", var2.toString());
+    rtn.replace("%3", var3.toString());
+    rtn.replace("%4", var4.toString());
+    return rtn;
 }
