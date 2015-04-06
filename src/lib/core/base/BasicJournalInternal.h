1984543228a6041b84151d1988f7dd995b2e95df
tony.dyndez@gmail.com <tony.dyndez@gmail.com@4f998d61-9b67-d9ff-1bf6-28367b1a3495>
2015-04-06 06:47:02 +0000

    More BasicJournal macros. [#23]

--- src/lib/core/base/BasicJournalInternal.h
+++ src/lib/core/base/BasicJournalInternal.h
@@ -7,6 +7,9 @@
 #include "BasicNameMap.h"
 
 #define _BJOURNAL (BasicJournalQueue::instance())
+#define _BJOURNAL_EXPR(expr) _BJOURNAL->expr
+
+#define _BJOURNAL_ADD(entry) _BJOURNAL_EXPR(add(entry))
 
 #define _BJENTRY(mac, cat) BasicJournalEntry() \
             << BasicJournalItem("TimeStamp", QDateTime::currentDateTime()) \
@@ -17,5 +20,10 @@
             << BasicJournalItem("MacroName", mac) \
             << BasicJournalItem("Category", cat) \
 
+#define _BJOURNAL_BOOL(mac, cat, expr, msg) \
+            _BJOURNAL_ADD(_BJENTRY(mac, cat)) \
+                << BasicJournalItem("Expression", expr) \
+                << BasicJournalItem("ExprName", #expr) \
+                << BasicJournalItem("Message", msg) \
 
 #endif // BASICJOURNALINTERNAL_H
